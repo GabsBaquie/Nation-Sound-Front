0fe18536e7fe58051005dcc715fe0ea867ed5655
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-09-04 19:44:27 +0200

    chore: Update PrincingBlock component to add an ID for anchor link

--- next-Front/components/blocks/Princing.tsx
+++ next-Front/components/blocks/Princing.tsx
@@ -18,7 +18,7 @@ const PrincingBlock: React.FC<PrincingProps> = ({ block }) => {
   const { title, text, plan } = block;
 
   return (
-    <section className="my-16">
+    <section className="my-16" id="princing">
       <div className="mb-10 text-left md:text-justify">
         <h1 className="mb-4 text-xl md:text-2xl">{title}</h1>
         <p className="text-sm md:text-lg">{text}</p>
