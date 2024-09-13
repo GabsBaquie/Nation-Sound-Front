45a126100e2d726812c56228dc8c92a7dc7a5af5
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-09-13 23:57:59 +0200

    Refactor: Update metadata in layout.tsx

--- next-Front/app/layout.tsx
+++ next-Front/app/layout.tsx
@@ -1,14 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Nation Sound",
-  description:
-    "Découvrez une programmation riche et éclectique dans un cadre idyllique",
-};
 
 export default function RootLayout({
   children,
@@ -17,6 +10,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Nation Sound</title>
+        <meta
+          name="description"
+          content="Découvrez une programmation riche et éclectique dans un cadre idyllique"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
