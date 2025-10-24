b8edae52f1286373dff1f4de001ef8023dfa0d26
GabsBaquie <GabsBaquie@users.noreply.github.com>
2025-10-24 10:56:55 +0200

    Refactor GaucheNav component for improved className handling
    - Simplified className logic in the GaucheNav component to enhance readability and maintainability.
    - Ensured consistent rendering behavior based on the isHomePage condition.

--- components/NavBar/gaucheNav.tsx
+++ components/NavBar/gaucheNav.tsx
@@ -16,9 +16,8 @@ const GaucheNav: React.FC<GaucheNavProps> = ({ className }) => {
 
   return (
     <div
-      className={`fixed flex flex-col w-12 gap-10 overflow-hidden border-r-2 md:w-auto md:px-8 border-border ${
-        isHomePage ? "block md:block" : "hidden md:block"
-      }`}>
+      className={`flex overflow-hidden fixed flex-col gap-10 w-12 border-r-2 md:w-auto md:px-8 border-border ${"hidden md:block"}`}
+    >
       <InteractiveScroll>
         <h2 className="flex flex-col items-center text-sm rotate-180 md:text-xl">
           {text.split("").map((char, index) => (
